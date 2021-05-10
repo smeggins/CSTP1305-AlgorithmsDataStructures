8471103e7a59f9cee4bf5a721350720d75320a35
Phillip Chadwick <phillipchadwick@hotmail.com>
2021-05-10 11:19:25 -0700

    adds Java LinkedList Implementation of stack

---
+++ StackJava/src/StackUsingLinkList.java
@@ -0,0 +1,51 @@
+class Node {
+    Node(int data) {
+        this.data = data;
+    }
+    int data;
+    Node next;
+}
+
+public class StackUsingLinkList {
+
+    StackUsingLinkList() {
+        top = null;
+    }
+
+    public Node top;
+
+    public void push(int a) {
+        
+        Node newNode = new Node(a);
+        newNode.next = null;
+        if (top == null) {
+            top = newNode;
+        }
+        else {
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+
+    public int pop() {
+        int nodeData = top.data;
+        top = top.next;
+        return nodeData;
+    }
+
+    public int peek() {
+        return top.data;
+    }
+
+    public static void main(String[] args) throws Exception {
+        StackUsingAnArray aStack = new StackUsingAnArray();
+         
+        aStack.push(1);
+        aStack.push(7);
+        aStack.push(2);
+        aStack.push(5);
+        aStack.pop();
+        aStack.push(4);
+        System.out.println(aStack.peek());
+    }
+}
